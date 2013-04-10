36f9112a8f4599fe74ffd0dc097e9fa9159a7301
Máximo Cuadros Ortiz <mcuadros@gmail.com>
2013-04-10 21:08:02 +0200

    skeleton

---
+++ src/Skeetr/Silex/Application.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * This file is part of the Skeetr package.
+ *
+ * (c) Máximo Cuadros <maximo@yunait.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Skeetr\Silex;
+use Silex\Application as BaseApplication;
+
+class Application extends BaseApplication
+{
+
+
+
+}
---
+++ tests/Skeetr/Tests/Silex/TestCase.php
@@ -0,0 +1,6 @@
+<?php
+namespace Skeetr\Tests\Silex;
+
+class TestCase extends \PHPUnit_Framework_TestCase {   
+
+}
